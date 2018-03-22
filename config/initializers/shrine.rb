7c993939fa36d30b864103e2bb2f6065f7f54e1c
Joe Minock <joe.minock@gmail.com>
2018-03-22 13:59:12 -0600

    Initial Commit

---
+++ config/initializers/shrine.rb
@@ -0,0 +1,10 @@
+require 'shrine/storage/file_system'
+
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'),
+}
+Shrine.plugin :activerecord
+Shrine.plugin :backgrounding
+Shrine::Attacher.promote { |data| PromoteJob.perform_later(data)}
+Shrine::Attacher.delete { |data| DeleteJob.perform_later(data)}
